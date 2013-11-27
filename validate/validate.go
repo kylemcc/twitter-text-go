ba6f02e297f4ca4bd01e39f75686c563d10377e9
Kyle McCullough <kylemcc@gmail.com>
2013-11-26 18:52:53 -0600

    validate tweet

--- validate/validate.go
+++ validate/validate.go
@@ -3,6 +3,7 @@ package validate
 
 import (
 	"code.google.com/p/go.text/unicode/norm"
+	"fmt"
 	"github.com/kylemcc/twitter-text-go/extract"
 	"strings"
 	"unicode/utf8"
@@ -12,9 +13,31 @@ const (
 	maxLength           = 140
 	shortUrlLength      = 22
 	shortHttpsUrlLength = 23
+	invalidChars        = "\uFFFE\uFEFF\uFFFF\u202A\u202B\u202C\u202D\u202E"
 )
 
 var formC = norm.NFC
+
+type TooLongError int
+
+func (e TooLongError) Error() string {
+	return fmt.Sprintf("Length %d exceeds %d characters", int(e), maxLength)
+}
+
+type EmptyError struct{}
+
+func (e EmptyError) Error() string {
+	return "Tweets may not be empty"
+}
+
+type InvalidCharacterError struct {
+	Character rune
+	Offset    int
+}
+
+func (e InvalidCharacterError) Error() string {
+	return fmt.Sprintf("Invalid chararcter [%s] found at byte offset %d", e.Character, e.Offset)
+}
 
 // Returns the length of the string as it would be displayed. This is equivilent to the length of the Unicode NFC
 // (See: http://www.unicode.org/reports/tr15). This is needed in order to consistently calculate the length of a
@@ -42,8 +65,28 @@ func TweetLength(text string) int {
 	return length
 }
 
+// Checks whether a string is a valid tweet and returns true or false
 func TweetIsValid(text string) bool {
-	return false
+	err := ValidateTweet(text)
+	return err == nil
+}
+
+// Checks whether a string is a valid tweet. Returns nil if the string
+// is valid. Otherwise, it returns an error in the following cases:
+//
+// - The text is too long
+// - The text is empty
+// - The text contains invalid characters
+func ValidateTweet(text string) error {
+	if text == "" {
+		return EmptyError{}
+	} else if length := TweetLength(text); length > maxLength {
+		return TooLongError(length)
+	} else if i := strings.IndexAny(text, invalidChars); i > -1 {
+		r, _ := utf8.DecodeRuneInString(text[i:])
+		return InvalidCharacterError{Offset: i, Character: r}
+	}
+	return nil
 }
 
 func UsernameIsValid(username string) bool {
